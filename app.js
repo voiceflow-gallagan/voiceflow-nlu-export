9b16b8d2ac327d3d349c35a8948376fb89b3b7a8
voiceflow-gallagan <nicolas@voiceflow.com>
2023-03-20 17:09:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const bodyParser = require('body-parser')
+const { parseExportData, generateCsv } = require('./utils')
+const fs = require('fs')
+const path = require('path')
+
+const app = express()
+app.use(bodyParser.json())
+
+// Serve the 'exports' folder as a static resource
+app.use('/exports', express.static(path.join(__dirname, 'exports')))
+
+app.post('/export', async (req, res) => {
+  try {
+    const { versionID, apiKey } = req.body
+
+    const response = await axios.get(
+      `https://api.voiceflow.com/v2/versions/${versionID}/export`,
+      {
+        headers: {
+          Authorization: apiKey,
+        },
+      }
+    )
+
+    const exportData = response.data
+    const { intents, slots } = parseExportData(exportData)
+    const csvContent = generateCsv(intents, slots)
+
+    res.setHeader('Content-Type', 'text/csv')
+    res.setHeader('Content-Disposition', 'attachment; filename=export.csv')
+    res.send(csvContent)
+
+    return
+    // Save the CSV content to a file in the exports subfolder
+    const filename = `export-${Date.now()}.csv`
+    const filePath = path.join(__dirname, 'exports', filename)
+    fs.writeFileSync(filePath, csvContent)
+
+    // Return the URL to download the CSV file
+    res
+      .status(200)
+      .json({ url: `${req.protocol}://${req.get('host')}/exports/${filename}` })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Error exporting data' })
+  }
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
---
+++ utils.js
@@ -0,0 +1,88 @@
+const { faker } = require('@faker-js/faker')
+
+function parseExportData(exportData) {
+  const intents = exportData.version.platformData.intents
+  const slots = exportData.version.platformData.slots
+
+  return { intents, slots }
+}
+
+function replaceSlotValues(utterance, slots) {
+  return utterance.replace(
+    /(?:\s*){{\[(.*?)\]\.(.*?)}}(?:\s*)/g,
+    (_, slotName, slotKey, offset) => {
+      const slot = slots.find((s) => s.key === slotKey)
+
+      if (slot) {
+        const spaceBefore = offset > 0 ? ' ' : ''
+        const spaceAfter = ' '
+
+        if (slot.type.value === 'VF.EMAIL') {
+          return spaceBefore + faker.internet.email() + spaceAfter
+        } else if (slot.type.value === 'VF.NAME') {
+          return spaceBefore + faker.name.firstName() + spaceAfter
+        } else if (slot.type.value === 'VF.NUMBER') {
+          return spaceBefore + faker.random.numeric(2) + spaceAfter
+        } else if (slot.type.value === 'VF.PHONENUMBER') {
+          return spaceBefore + faker.phone.phoneNumber() + spaceAfter
+        } else if (slot.type.value === 'VF.URL') {
+          return spaceBefore + faker.internet.url() + spaceAfter
+        } else if (slot.type.value === 'VF.PERCENTAGE') {
+          return spaceBefore + `${faker.random.numeric(2)}%` + spaceAfter
+        } else if (slot.type.value === 'VF.CURRENCY') {
+          return spaceBefore + `$${faker.finance.amount()}` + spaceAfter
+        } else if (slot.type.value === 'VF.DATETIME') {
+          return spaceBefore + faker.date.recent().toISOString() + spaceAfter
+        } else if (slot.type.value === 'VF.ORDINAL') {
+          return spaceBefore + ordinal(faker.random.numeric(2)) + spaceAfter
+        } else if (slot.type.value === 'VF.KEY_PHRASE') {
+          return spaceBefore + faker.random.word() + spaceAfter
+        } else if (slot.type.value === 'VF.AGE') {
+          return spaceBefore + `${faker.random.numeric(2)} years` + spaceAfter
+        } else if (slot.type.value === 'VF.DIMENSION') {
+          return spaceBefore + `${faker.random.numeric(2)} feet` + spaceAfter
+        } else if (slot.type.value === 'VF.TEMPERATURE') {
+          return (
+            spaceBefore +
+            `${faker.random.numeric(2)} degrees Celsius` +
+            spaceAfter
+          )
+        } else if (slot.type.value === 'VF.GEOGRAPHY') {
+          return spaceBefore + faker.address.city() + spaceAfter
+        } else {
+          const inputs = slot.inputs.flatMap((input) => input.split(','))
+          const inputIndex = Math.floor(Math.random() * inputs.length)
+          const slotValue = inputs[inputIndex]
+          return spaceBefore + slotValue + spaceAfter
+        }
+      }
+      return ''
+    }
+  )
+}
+
+function ordinal(n) {
+  const s = ['th', 'st', 'nd', 'rd']
+  const v = n % 100
+  return n + (s[(v - 20) % 10] || s[v] || s[0])
+}
+
+function generateCsv(intents, slots) {
+  let rows = []
+
+  intents.forEach((intent) => {
+    intent.inputs.forEach((input) => {
+      const utterance = replaceSlotValues(input.text, slots)
+        .trim()
+        .replace(/\s{2,}/g, ' ')
+      const row = [intent.name, utterance]
+        .map((field) => `"${field.replace(/"/g, '""')}"`)
+        .join(',')
+      rows.push(row)
+    })
+  })
+
+  return rows.join('\n')
+}
+
+module.exports = { parseExportData, generateCsv }
